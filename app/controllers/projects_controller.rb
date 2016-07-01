b770f06bc8173692f61ba3fd6d74074d85b99d24
Rayed S <siddiqui.rayed@gmail.com>
2016-07-01 23:32:43 +0000

    added video capabilities for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -46,7 +46,7 @@ class ProjectsController < ApplicationController
   end
 
   def project_params
-    params.require(:project).permit(:title, :description, :link, :slug)
+    params.require(:project).permit(:title, :description, :link, :video, :slug)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701024051) do
+ActiveRecord::Schema.define(version: 20160701220206) do
 
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160701024051) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "slug"
+    t.string   "video"
   end
 
   add_index "projects", ["slug"], name: "index_projects_on_slug", unique: true
